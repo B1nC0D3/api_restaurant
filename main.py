012ddcd15931af212ee9db9fa875dc63a0f47236
Kirill Kudryashov <b1nanon@mail.ru>
2023-01-21 14:21:53 +0300

    Added CORS middleware

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from apiv1 import routes_v1
+from settings import settings
 
 app = FastAPI()
 
+
+app.add_middleware(
+        CORSMiddleware,
+        allow_origins=settings.allowed_origins,
+        allow_credentials=True,
+        allow_methods=['*'],
+        allow_headers=['*'],
+)
 app.include_router(routes_v1.router)
--- settings.py
+++ settings.py
@@ -3,6 +3,7 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     database_url: str
+    allowed_origins: set
 
 
 settings = Settings(
