61bd7d14c48b584c2ff102a3b27aac1ca5c63631
Kirill Kudryashov <b1nanon@mail.ru>
2023-01-26 16:48:34 +0300

    Added all schemas to one file

--- apiv1/models/base.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-
-class BaseItem(BaseModel):
-    title: str
-    description: str
--- apiv1/models/menu.py
+++ apiv1/models/menu.py
@@ -1,4 +1,47 @@
-from .base import BaseItem
+from decimal import Decimal
+
+from pydantic import BaseModel
+
+
+class BaseItem(BaseModel):
+    title: str
+    description: str
+
+
+class BaseDish(BaseItem):
+    price: Decimal
+
+
+class DishCreate(BaseDish):
+    pass
+
+
+class DishUpdate(BaseDish):
+    pass
+
+
+class DishResponse(BaseDish):
+    id: int
+    price: str
+
+    class Config:
+        orm_mode = True
+
+
+class SubmenuCreate(BaseItem):
+    pass
+
+
+class SubmenuUpdate(BaseItem):
+    pass
+
+
+class SubmenuResponse(BaseItem):
+    id: int
+    dishes: list[DishResponse]
+
+    class Config:
+        orm_mode = True
 
 
 class MenuCreate(BaseItem):
@@ -10,9 +53,8 @@ class MenuUpdate(BaseItem):
 
 
 class MenuResponse(BaseItem):
-    id: str
-    submenus_count: int
-    dishes_count: int
+    id: int
+    submenus: list[SubmenuResponse]
 
     class Config:
         orm_mode = True
