4bf8cae3a0d53bbcd30da863568a7bb02ba290ea
Kirill Kudryashov <b1nanon@mail.ru>
2023-01-27 15:59:57 +0300

    integrated authentication to menus

--- apiv1/routes/menus.py
+++ apiv1/routes/menus.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, Depends, status
 
+from apiv1.models.auth import UserResponse
 from apiv1.models.menu import MenuCreate, MenuResponse, MenuUpdate
+from apiv1.services.auth import get_current_user
 from apiv1.services.menu_operations import MenuService
 
 router = APIRouter(
@@ -8,8 +10,9 @@ router = APIRouter(
 
 
 @router.get('/', response_model=list[MenuResponse])
-async def get_menus(menu_service: MenuService = Depends()):
-    return await menu_service.get_many()
+async def get_menus(menu_service: MenuService = Depends(),
+                    user: UserResponse = Depends(get_current_user)):
+    return await menu_service.get_many(user.id)
 
 
 @router.get('/{menu_id}', response_model=MenuResponse)
@@ -18,16 +21,19 @@ async def get_menu(menu_id: int, menu_service: MenuService = Depends()):
 
 
 @router.post('/', response_model=MenuResponse,
-             status_code=status.HTTP_201_CREATED)
-async def create_menu(menu_data: MenuCreate, menu_service: MenuService = Depends()):
-    return await menu_service.create(menu_data)
+             status_code=status.HTTP_201_CREATED,)
+async def create_menu(menu_data: MenuCreate, menu_service: MenuService = Depends(),
+                      user: UserResponse = Depends(get_current_user)):
+    return await menu_service.create(user.id, menu_data)
 
 
 @router.patch('/{menu_id}', response_model=MenuResponse)
-async def update_menu(menu_id: int, menu_data: MenuUpdate, menu_service: MenuService = Depends()):
-    return await menu_service.update(menu_id, menu_data)
+async def update_menu(menu_id: int, menu_data: MenuUpdate, menu_service: MenuService = Depends(),
+                      user: UserResponse = Depends(get_current_user)):
+    return await menu_service.update(menu_id, user.id, menu_data)
 
 
 @router.delete('/{menu_id}', status_code=status.HTTP_204_NO_CONTENT)
-async def delete_menu(menu_id: int, menu_service: MenuService = Depends()):
-    await menu_service.delete(menu_id)
+async def delete_menu(menu_id: int, menu_service: MenuService = Depends(),
+                      user: UserResponse = Depends(get_current_user)):
+    await menu_service.delete(menu_id, user.id)
--- apiv1/services/menu_operations.py
+++ apiv1/services/menu_operations.py
@@ -12,31 +12,35 @@ class MenuService(BaseService):
     async def get(self, menu_id: int) -> Menu:
         return await self._get(menu_id)
 
-    async def get_many(self) -> list[Menu]:
+    async def get_many(self, user_id: int) -> list[Menu]:
         menus = await self.session.execute(
                 select(Menu)
+                .filter(Menu.user_id == user_id)
                 .options(selectinload(Menu.submenus)))
         menus = menus.unique().scalars().all()
         return menus
 
-    async def create(self, menu_data: MenuCreate) -> Menu:
+    async def create(self, user_id: int, menu_data: MenuCreate) -> Menu:
         menu = Menu(
-            **menu_data.dict()
+            **menu_data.dict(),
+            user_id=user_id
         )
         self.session.add(menu)
         await self.session.commit()
         menu = await self._get(menu.id)
         return menu
 
-    async def update(self, menu_id: int, menu_data: MenuUpdate) -> Menu:
+    async def update(self, menu_id: int, user_id: int, menu_data: MenuUpdate) -> Menu:
         menu = await self._get(menu_id)
+        await self._is_author(user_id, menu.user_id)
         for key, value in menu_data:
             setattr(menu, key, value)
         await self.session.commit()
         return menu
 
-    async def delete(self, menu_id: int) -> None:
+    async def delete(self, menu_id: int, user_id: int) -> None:
         menu = await self._get(menu_id)
+        await self._is_author(user_id, menu.user_id)
         await self.session.delete(menu)
         await self.session.commit()
 
@@ -50,3 +54,9 @@ class MenuService(BaseService):
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                                 detail='menu not found')
         return menu
+
+    async def _is_author(self, user_id: int, author_id: int):
+        if user_id != author_id:
+            raise HTTPException(
+                    status_code=status.HTTP_400_BAD_REQUEST,
+                    detail='You are not the author')
