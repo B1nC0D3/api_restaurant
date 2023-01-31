94c00b1e176f3a76e2866bf6ac691a3ccf60b2ba
Kirill Kudryashov <b1nanon@mail.ru>
2023-02-01 01:35:13 +0300

    added alembic migrations

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from apiv1 import routes_v1
-from database.database import Base, engine
 from settings import settings
 
 app = FastAPI()
@@ -16,10 +15,3 @@ app.add_middleware(
     allow_headers=['*'],
 )
 app.include_router(routes_v1.router)
-
-
-@app.on_event('startup')
-async def init_tables():
-    async with engine.begin() as connection:
-        await connection.run_sync(Base.metadata.drop_all)
-        await connection.run_sync(Base.metadata.create_all)
