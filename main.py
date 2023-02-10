71797e90098d7fea328d33007ccff07aa334a4d4
Kirill Kudryashov <b1nanon@mail.ru>
2023-02-10 14:33:56 +0300

    rewrite CORS settigs to debug

--- main.py
+++ main.py
@@ -3,15 +3,14 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from apiv1 import routes_v1
 from database.database import engine, Base
-from settings import settings
 
 app = FastAPI()
 
 
 app.add_middleware(
         CORSMiddleware,
-        allow_origins=settings.allowed_origins,
-        allow_credentials=True,
+        allow_origins='*',
+        allow_credentials=False,
         allow_methods=['*'],
         allow_headers=['*'],
 )
